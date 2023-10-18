64cae17308da0682bb2f44e974c76b403266334f
Qing Xin <117796936+Qingxin97@users.noreply.github.com>
2023-10-18 19:09:09 +0800

    Add files via upload

---
+++ EOD_2023.10.18_Webtool_Data_cleaning_.py
@@ -0,0 +1,334 @@
+# Libraries
+import numpy as np
+import pandas as pd
+import streamlit as st
+import scipy.stats as sp
+import matplotlib.pyplot as plt
+import seaborn as sns
+import base64
+import io
+
+# Webtool sidebar
+st.title('Data cleaning webtool')
+st.write('Made by Group 1: Shree Pooja, Qing Xin, Vinaya Venkat, He Shan')
+
+st.sidebar.header('Welcome to the data cleaning webtool')
+st.sidebar.write('Easily scroll through the webtool by pressing the link below')
+st.sidebar.markdown('[Uploading your datafile](#upload-anchor)') 
+st.sidebar.markdown('[Managing duplicate values](#manage-duplicates)')
+st.sidebar.markdown('[Managing missing values](#manage-missing)')
+st.sidebar.markdown('[Data type converter](#convert-int-to-decimal)')
+st.sidebar.markdown('[Split or concatenate columns](#split-concatenate-columns)')
+st.sidebar.markdown('[Date-to-Gene converter](#convert-dates-to-gene-names)')  # Provided the link to the gene-to-date converter
+show_docs = st.sidebar.checkbox('Check documentation')  # Need to add more documentation - complete demo with snapshots  
+
+# Section 1:  Doucmentation
+if show_docs:
+    st.title("Data Cleaner User Manual")
+    st.subheader("Data Cleaner is a web tool that serves as a one-stop solution for your large data files from your research")
+    st.write("With Data Cleaner, you can perform multiple data cleaning procedures by simply uploading your file and selecting how you would like your data to be cleaned.")
+    st.write("Currently, we provide the following functions:")
+    st.write("1. Duplicate value management")
+    st.write("2. Missing value management")
+    st.write("3. Data type conversion")
+    st.write("4. Column splitting and concatenation")
+    st.write("5. Date-to-Gene conversion (by Dr. Chan Kuan Rong)")
+    st.subheader("How do I use this web tool?")
+    st.write("To get yourself started, first you should choose a data file type that you want to be cleaned. We allow you to upload .xls, .xlsx, .csv, and .tsv files. Make sure that your file is a long file instead of a wide file.")
+
+# Section 2: Upload datafile
+st.markdown('<a name="upload-anchor"></a>', unsafe_allow_html=True)  # Create an anchor for this section
+st.subheader('1. Uploading your datafile')
+
+option = st.selectbox('Please choose the file format you are uploading', ('None','Excel', 'CSV', 'TSV'))
+st.write('You have selected:', option)
+
+upload = st.file_uploader(f'Upload your file here')
+
+st.sidebar.subheader('*Please note:*')
+st.sidebar.write('*1. The webtool will consider the first column in the file as the index*')
+st.sidebar.write('*2. Add an Excel file with only one sheet in it to prevent errors*')
+
+# Convert the uploaded file to a DataFrame
+df = None
+
+if upload is not None:
+    if option == 'Excel':
+        df = pd.read_excel(upload, index_col = 0)
+    elif option == 'CSV':
+        df = pd.read_csv(upload, index_col = 0)
+    elif option == 'TSV':
+        df = pd.read_csv(upload, sep='\t', index_col = 0)
+else:
+    # Load an example datafile 
+    url = "https://raw.githubusercontent.com/VinayaVenkat26/GSMV6907-Group1/main/Streamlit%20demo%20dataset.xlsx"
+    df = pd.read_excel(url,index_col = 0) 
+    st.write('No file uploaded. Showing example data.')
+    st.write(df)
+    
+   
+
+
+
+# Section 3: Check for duplicate values in row titles
+st.markdown('<a name="manage-duplicates"></a>', unsafe_allow_html=True)  # Create an anchor for this section
+st.subheader('2. Managing duplicate values')
+
+# Function to check for duplicates in a specified column or index
+def check_duplicates_in_column_or_index(df, column_name, use_index=False):
+    if df is not None and not df.empty:
+        if use_index:
+            index_dups = df.index.duplicated().any()
+            if index_dups:
+                duplicated_values = df[df.index.duplicated(keep=False)]
+                st.write('Duplicate values in index:')
+                st.write(duplicated_values)
+                return 'Duplicate values in index found.'
+            else:
+                return 'No duplicate values in index were found.'
+        else:
+            if column_name in df.columns:
+                col_dups = df[column_name].duplicated().any()
+                if col_dups:
+                    duplicated_values = df[df[column_name].duplicated(keep=False)]
+                    st.write(f'Duplicate values in column "{column_name}":')
+                    st.write(duplicated_values)
+                    return f'Duplicate values in column "{column_name}" found.'
+                else:
+                    return f'No duplicate values in column "{column_name}" were found.'
+            else:
+                return f'Column "{column_name}" not found in the DataFrame.'
+
+# Function to handle duplicates in a specified column or index
+def handle_duplicates_in_column_or_index(df, column_name, selected_action, use_index=False):
+    handled_successfully = False  # Initialize a flag
+
+    if df is not None and not df.empty:
+        if use_index:
+            if selected_action == 'Choose only the first value':
+                df = df.loc[~df.index.duplicated(keep='first')]
+                st.write('Only the first value in index kept.')
+                handled_successfully = True  # Set the flag to True
+            elif selected_action == 'Choose only the last value':
+                df = df.loc[~df.index.duplicated(keep='last')]
+                st.write('Only the last value in index kept.')
+                handled_successfully = True  # Set the flag to True
+            elif selected_action == 'Ignore':
+                st.write('No action taken for duplicates in index.')
+        else:
+            if selected_action == 'Take mean of duplicates' and column_name in df.select_dtypes(include=['float64', 'int64']).columns:
+                df[column_name] = df.groupby(column_name)[column_name].transform('mean')
+                st.write(f'Mean of duplicates in column "{column_name}" taken.')
+                handled_successfully = True  # Set the flag to True
+            elif selected_action == 'Choose only the first value':
+                df = df[~df[column_name].duplicated(keep='first')]
+                st.write(f'Only the first value in column "{column_name}" kept.')
+                handled_successfully = True  # Set the flag to True
+            elif selected_action == 'Choose only the last value':
+                df = df[~df[column_name].duplicated(keep='last')]
+                st.write(f'Only the last value in column "{column_name}" kept.')
+                handled_successfully = True  # Set the flag to True
+            elif selected_action == 'Ignore':
+                st.write(f'No action taken for duplicates in column "{column_name}".')
+
+    return df, handled_successfully  # Return the DataFrame and the flag
+
+# User selection
+index_name = df.index.name if df.index.name else "[Index]"
+all_columns_plus_index = list(df.columns) + [index_name]
+
+selected_column_or_index = st.selectbox('Select a column or the index to check for duplicates:', all_columns_plus_index)
+
+use_index = (selected_column_or_index == index_name)
+if use_index:
+    st.write(check_duplicates_in_column_or_index(df, None, use_index=True))
+else:
+    st.write(check_duplicates_in_column_or_index(df, selected_column_or_index, use_index=False))
+
+# b. Handle duplicates
+if st.checkbox('Handle duplicates in this column or index'):
+    if use_index:
+        selected_action = st.selectbox(
+            'How would you like to handle duplicates in the index?',
+            ('Choose only the first value', 'Choose only the last value', 'Ignore')
+        )
+    else:
+        selected_action = st.selectbox(
+            f'How would you like to handle duplicates in column "{selected_column_or_index}"?',
+            ('Take mean of duplicates', 'Choose only the first value', 'Choose only the last value', 'Ignore')
+        )
+    df, handled_successfully = handle_duplicates_in_column_or_index(df, selected_column_or_index, selected_action, use_index)
+    if handled_successfully:
+        st.subheader("Cleaned DataFrame:")
+        st.dataframe(df)
+
+
+
+# Section 4: Manage missing values
+st.markdown('<a name="manage-missing"></a>', unsafe_allow_html=True)  # Create an anchor for this section
+st.subheader('3. Managing missing values')
+
+def handle_missing_values(df):
+    missing_rows = df[df.isnull().any(axis=1)]
+    if not missing_rows.empty:
+        st.write("Rows with missing values:")
+        st.dataframe(missing_rows)
+        
+        # Add options for the user
+        st.subheader("Options:")
+        option = st.radio("Select an action:", ("Delete Rows with Missing Values", "Fill Missing Values"))
+        
+        if option == "Delete Rows with Missing Values":
+            df = df.dropna(axis=0, how = 'any')
+            st.write("Rows with missing values deleted.")
+        else:
+            # Allow the user to specify a value for filling missing values
+            fill_value = st.text_input("Enter a value to fill missing values:")
+            if st.button("Fill Missing Values"):
+                df = df.fillna(fill_value)
+                st.write("Missing values filled with the specified value.")
+
+        # Display the cleaned DataFrame (either with deleted rows or filled values)
+        st.subheader("Cleaned DataFrame:")
+        st.dataframe(df)
+    else:
+        st.write("No rows with missing values found.")
+        
+    return df
+
+# call the function
+df = handle_missing_values(df)
+
+
+
+# Section 5: Integer to decimal conversion and vice versa
+st.subheader('4. Data type converter')
+st.markdown('<a name="convert-int-to-decimal"></a>', unsafe_allow_html=True)  # Create an anchor for this section
+
+selected_option_convert = st.selectbox("Would you like to convert data types in your file?", ["NO", "YES"])
+
+if selected_option_convert == "YES":
+    selected_conversion_type = st.selectbox("What conversion type would you like to perform?", ["Convert to Integers", "Convert to Floats", "Convert to Strings"])
+    columns_to_convert = st.multiselect("Select the columns to convert:", df.columns)
+
+    if columns_to_convert:
+        if selected_conversion_type == "Convert to Floats":
+            df[columns_to_convert] = df[columns_to_convert].astype(float)
+
+        elif selected_conversion_type == "Convert to Integers":
+            # Check for NaN or infinite values in the columns
+            if df[columns_to_convert].isnull().any().any() or np.isinf(df[columns_to_convert]).any().any():
+                action = st.radio("NaN or infinite values detected. How would you like to handle them?",
+                                  ["Replace with specific value", "Drop rows containing NaN or inf", "Do nothing"])
+                if action == "Replace with specific value":
+                    replace_val = st.number_input("Enter the value to replace NaN or infinite values with:", value=0)
+                    df[columns_to_convert] = df[columns_to_convert].fillna(replace_val).replace([np.inf, -np.inf], replace_val)
+                elif action == "Drop rows containing NaN or inf":
+                    df.dropna(subset=columns_to_convert, inplace=True)
+                    df = df[~np.isinf(df[columns_to_convert]).any(axis=1)]
+            df[columns_to_convert] = df[columns_to_convert].astype(int)
+
+        elif selected_conversion_type == "Convert to Strings":
+            df[columns_to_convert] = df[columns_to_convert].astype(str)
+
+        st.write(df)
+    else:
+        st.write("Please select the columns you wish to convert.")
+
+else:
+    st.write("We will NOT be converting data types in your file")
+
+
+
+
+
+# Section 6: Split or concatenate columns
+st.markdown('<a name="split-concatenate-columns"></a>', unsafe_allow_html=True)  # Create an anchor for this section
+st.subheader('5. Split or concatenate columns')
+
+# Code for splitting or concatenating columns
+st.subheader('Options:')
+operation = st.radio("Select an action:", ("Split", "Concatenate"))
+
+if operation == "Split":
+        # Split columns
+    st.subheader("Split Columns")
+    column_to_split = st.selectbox("Select the column to split:", df.columns)
+    separator = st.text_input("Separator for splitting:", ",")
+        
+        # Ensure the selected column is treated as a string before splitting
+    split_values = df[column_to_split].astype(str).str.split(separator, expand=True)
+
+        # Label the split columns with the original column name
+    split_values.columns = [f"{column_to_split}_{i + 1}" for i in range(split_values.shape[1])]
+
+        # Display the split values along with the original dataset
+    st.write(split_values)
+    st.write("Merged Dataset with Split Columns")
+    merged_df = pd.concat([df, split_values], axis=1)
+    st.write(merged_df)
+
+else:
+        # Concatenate columns
+    st.subheader("Concatenate Columns")
+    st.write("Select the columns to concatenate:")
+    concat_columns = st.multiselect("Columns to concatenate:", df.columns)
+    separator = st.text_input("Separator for concatenation:", " ")
+    concat_df = df[concat_columns].astype(str).agg(separator.join, axis=1)
+
+        # Display the concatenated values along with the original dataset
+    st.write("Concatenated Values")
+    st.write(concat_df)
+    st.write("Merged Dataset with Concatenated Column")
+    merged_df = pd.concat([df, concat_df.rename("Concatenated_Column")], axis=1)
+    st.write(df)
+
+    # Set the value of the action variable
+action = st.radio("Choose action:", ("Keep Selected Columns", "Delete Selected Columns"))
+
+# Continue with the action based on the user's choice
+if action == "Keep Selected Columns":
+    columns_to_keep = st.multiselect("Please select all columns to keep:", merged_df.columns)
+    result_df = merged_df[columns_to_keep]
+else:
+    columns_to_delete = st.multiselect("Please select all columns to delete:", merged_df.columns)
+    result_df = merged_df.drop(columns=columns_to_delete)
+
+# Display the resulting dataset
+    st.subheader("Resulting Dataset")
+    st.write(result_df)
+    
+
+# Section 7: Conclusion , retieving the data from the webtool
+
+# Function to create a download link for the processed data
+def get_binary_file_downloader_html(df):
+
+    # Create a BytesIO object to store the Excel file
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=True)
+    
+    b64 = base64.b64encode(output.getvalue()).decode()
+    href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="cleaned_data.xlsx">Download Excel File</a>'
+    return href
+
+st.subheader('All done!!')
+st.write('Your datafile has been cleaned')
+st.write("You can download the processed data as an Excel file.")
+st.markdown(get_binary_file_downloader_html(result_df), unsafe_allow_html=True)
+
+
+# Section 8: Convert dates to gene names
+st.markdown('<a name="convert-dates-to-gene-names"></a>', unsafe_allow_html=True)  # Create an anchor for this section
+st.subheader('Extra resource -  Date-to-Gene Tool')
+
+st.write("If you want to perform a Date-to-Gene conversion in your file, you can click the link below to launch the Date-to-Gene tool made by Dr. Chan Kuan Rong")
+
+st.write("https://share.streamlit.io/kuanrongchan/date-to-gene-converter/main/date_gene_tool.py")
+
+
+
+
+
+
