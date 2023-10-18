866f56919c3092d9ed1f9b04fe722e857a258a20
hs991216 <49970775+hs991216@users.noreply.github.com>
2023-10-18 17:00:01 +0800

    Update 2023.10.16cleaning_data_tool.py

--- 2023.10.16cleaning_data_tool.py
+++ 2023.10.16cleaning_data_tool.py
@@ -6,6 +6,21 @@ import scipy.stats as sp
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+#Documentation
+docu = st.sidebar.checkbox("Documentation")
+
+if docu:
+    st.title("Data Cleaner User Manual")
+    st.subheader("Data Cleaner is a web tool that serves as a one-stop solution for your large data files from your research")
+    st.write("With Data Cleaner, you can perform multiple data cleaning procedures by simply uploading your file and selecting how you would like your data to be cleaned.")
+    st.write("Currently, we provide the following functions:")
+    st.write("1. Duplicate value management")
+    st.write("2. Missing value management")
+    st.write("3. Data type conversion")
+    st.write("4. Column splitting and concatenation")
+    st.write("5. Date-to-Gene conversion (by Dr. Chan Kuan Rong)")
+    st.subheader("How do I use this web tool?")
+    st.write("To get yourself started, first you should choose a data file type that you want to be cleaned. We allow you to upload .xls, .xlsx, .csv, and .tsv files. Make sure that your file is a long file instead of a wide file.")
 
 # Information on the webtool
 st.title('Data cleaning webtool')
